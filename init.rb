4f5b6a3d5e89fc108e5855ab6fa458b12e7eddd1
UruruSakuya <atomic.transfer.comp@gmail.com>
2014-05-17 07:55:20 +0900

    スケルトン作成、プラグイン情報を変更し、Redmineから認識できるところまでをコミット。

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_descendant_counter do
+  name 'Redmine Descendant Counter plugin'
+  author 'Ururu Sakuya'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/UruruSakuya/redmine_descendant_counter'
+  author_url 'http://ururusakuya.wordpress.com/'
+end
