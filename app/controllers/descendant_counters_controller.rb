d3301072e67dd385570a384851886be20562504a
UruruSakuya <atomic.transfer.comp@gmail.com>
2014-05-17 08:35:32 +0900

    プロジェクトメニューへ追加、index表示までをコミット。

---
+++ app/controllers/descendant_counters_controller.rb
@@ -0,0 +1,13 @@
+class DescendantCountersController < ApplicationController
+  unloadable
+
+
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- init.rb
+++ init.rb
@@ -5,4 +5,15 @@ Redmine::Plugin.register :redmine_descendant_counter do
   version '0.0.1'
   url 'https://github.com/UruruSakuya/redmine_descendant_counter'
   author_url 'http://ururusakuya.wordpress.com/'
+
+  project_module :counter do
+    permission :view_descendant_counter, :descendant_counters => [:index]
+    permission :manage_decendant_counter, :descendant_counters => [:new, :edit, :create, :update, :destroy],
+               :require => :member
+  end
+
+  menu :project_menu, :descendant_counter,
+       {:controller => 'descendant_counters', :action =>'index'},
+       :param => :project_id
+
 end
